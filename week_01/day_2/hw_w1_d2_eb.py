fc8c005902702f44aff4471a306bf0b919ed02d0
John Panton <johnpanton29@outlook.com>
2022-11-22 16:06:23 +0000

    A bit more done on excersise b

--- week_01/day_2/hw_w1_d2_eb.py
+++ week_01/day_2/hw_w1_d2_eb.py
@@ -57,18 +57,34 @@ users = {
 print(users["Jonathan"].get('twitter'))
 # 2. Get Erik's hometown
 print(users["Erik"].get('home_town'))
+
 # 3. Get the list of Erik's lottery numbers
 print(users["Erik"].get('lottery_numbers'))
+
 # 4. Get the species of Avril's pet Monty
 print(users["Avril"]['pets'])
+
 # 5. Get the smallest of Erik's lottery numbers
-# list=[]
-# list.extend=users["Erik"].get('lottery_numbers')
-# print(list)
-# list.sort()
+erikLottery=[]
+erikLottery=users["Erik"].get('lottery_numbers')
+erikLottery.sort()
+print(erikLottery[0])
+
 # 6. Return an list of Avril's lottery numbers that are even
+avrilLottery=[]
+avrilLottery=users["Avril"].get('lottery_numbers')
+for num in avrilLottery:
+    if num % 2==0:
+        print(num)
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
+lotteryChange=[]
+lotteryChange=users["Erik"].get('lottery_numbers')
+lotteryChange.append(7)
+users["Erik"]["lottery_numbers"]=lotteryChange
+print(users["Erik"]["lottery_numbers"])  
 # 8. Change Erik's hometown to Edinburgh
+users['Erik']['home_town']='Edinburgh'
+print(users['Erik']['home_town'])
 # 9. Add a pet dog to Erik called "fluffy"
 # 10. Add another person to the users dictionary
 
